048c474b8c3fb7db9900ed0a0b323c3c62caa310
Mike-Tranzit <mihail.devyatov@port-tranzit.ru>
2019-11-14 11:52:16 +0300

    fix(Travis): git add -A #1

--- travis/push.sh
+++ travis/push.sh
@@ -1,10 +1,1 @@
 #!/bin/sh
-
-git config --global user.email "travis@travis-ci.org"
-git config --global user.name "Travis CI"
-git remote add origin https://${GITHUB_TOKEN}@github.com/Mike-Tranzit/AudioPlayer > /dev/null 2>&1
-git add -A
-# We don’t want to run a build for a this commit in order to avoid circular builds:
-# add [ci skip] to the git commit message
-git commit -m "Snapshot autobuild N.$TRAVIS_BUILD_NUMBER [ci skip]"
-git push origin
