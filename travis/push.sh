d90874bd4fef4dd1352188de8b14dff98cb90b17
Mike-Tranzit <mihail.devyatov@port-tranzit.ru>
2019-11-14 19:20:44 +0300

    Merge remote-tracking branch 'origin/master'

--- travis/push.sh
+++ travis/push.sh
@@ -1,10 +1,33 @@
 #!/bin/sh
 echo "push.sh was touched"
-git config --global user.email "travis@travis-ci.org"
-git config --global user.name "Travis CI"
-git remote add origin https://$AUTOBUILD_TOKEN@github.com/Mike-Tranzit/AudioPlayer.git > /dev/null 2>&1
-git add -A
-# We don’t want to run a build for a this commit in order to avoid circular builds:
-# add [ci skip] to the git commit message
-git commit -m "Snapshot autobuild N.$TRAVIS_BUILD_NUMBER [ci skip]"
-git push origin
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+  echo "config was set"
+}
+
+commit_country_json_files() {
+  git checkout master
+  # Current month and year, e.g: Apr 2018
+  dateAndMonth=`date "+%b %Y"`
+  # Stage the modified files in dist/output
+  git add -A
+  # Create a new commit with a custom build message
+  # with "[skip ci]" to avoid a build loop
+  # and Travis build number for reference
+  git commit -m "Travis update: $dateAndMonth (Build $TRAVIS_BUILD_NUMBER)" -m "[skip ci]"
+  echo "commit is ready"
+}
+
+upload_files() {
+  # Remove existing "origin"
+  git remote rm origin
+  # Add new "origin" with access token in the git URL for authentication
+  git remote add origin https://Mike-Tranzit:${GH_TOKEN}@github.com/Mike-Tranzit/AudioPlayer.git > /dev/null 2>&1
+  git push origin master --quiet
+  echo "push!!!!"
+}
+
+setup_git
+commit_country_json_files
+upload_files
