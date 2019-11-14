1bd98d14703734dd4515ad3d7d6040ed8ec2e932
Mike-Tranzit <mihail.devyatov@port-tranzit.ru>
2019-11-14 19:11:22 +0300

    Merge remote-tracking branch 'origin/master'

--- travis/push.sh
+++ travis/push.sh
@@ -2,7 +2,7 @@
 echo "push.sh was touched"
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
-git remote add origin https://${AUTOBUILD_TOKEN}@github.com/Mike-Tranzit/AudioPlayer.git > /dev/null 2>&1
+git remote add origin https://$AUTOBUILD_TOKEN@github.com/Mike-Tranzit/AudioPlayer.git > /dev/null 2>&1
 git add -A
 # We don’t want to run a build for a this commit in order to avoid circular builds:
 # add [ci skip] to the git commit message
