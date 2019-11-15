a3501abad4dc4f95e31003abbe6110e3a03b6b9a
Mike-Tranzit <mihail.devyatov@port-tranzit.ru>
2019-11-15 11:03:45 +0300

    Merge remote-tracking branch 'origin/master'

--- travis/push.sh
+++ travis/push.sh
@@ -3,20 +3,31 @@ echo "push.sh was touched"
 setup_git() {
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis CI"
-  echo "config was set"
+  msg " CONFIG WAS SET!"
 }
 
 commit_country_json_files() {
-  git checkout master
+  if ! git checkout master; then
+        err "failed to checkout master"
+        return 1
+  fi
   # Current month and year, e.g: Apr 2018
   dateAndMonth=`date "+%b %Y"`
   # Stage the modified files in dist/output
-  git add -A
+  if ! git add -A; then
+        err "failed to add modified files to git index"
+        return 1
+  fi
+
   # Create a new commit with a custom build message
   # with "[skip ci]" to avoid a build loop
   # and Travis build number for reference
-  git commit -m "Travis update: $dateAndMonth (Build $TRAVIS_BUILD_NUMBER)" -m "[skip ci]"
-  echo "commit is ready"
+  if ! git commit -m "Travis update: $dateAndMonth (Build $TRAVIS_BUILD_NUMBER)" -m "[skip ci]"; then
+        err "failed to commit updates"
+        return 1
+  fi
+
+  msg " COMMIT IS READY!"
 }
 
 upload_files() {
@@ -25,7 +36,15 @@ upload_files() {
   # Add new "origin" with access token in the git URL for authentication
   git remote add origin https://Mike-Tranzit:${GH_TOKEN}@github.com/Mike-Tranzit/AudioPlayer.git > /dev/null 2>&1
   git push origin master --quiet
-  echo "push!!!!"
+  msg " PUSH WAS SUCCESS!"
+}
+
+err() {
+    msg "$*" 1>&2
+}
+
+msg() {
+    echo "TRAVIS-COMMIT: $*"
 }
 
 setup_git
