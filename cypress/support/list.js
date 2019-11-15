3480922b285ad225bca5b939cc56c095b3981c5c
Mike-Tranzit <mihail.devyatov@port-tranzit.ru>
2019-11-15 12:46:07 +0300

    clear

--- cypress/support/list.js
+++
@@ -1,5 +0,0 @@
-/* Первый элемент списка песен */
-Cypress.Commands.add('clickByFirtsTrack', _ => {
-    cy.get('ion-list > ion-item').first().as('firstTrack');
-    cy.get('@firstTrack').click();
-});
